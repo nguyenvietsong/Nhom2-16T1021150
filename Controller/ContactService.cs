86bf13478c565165bd06325b32fda1f7d9dc415c
SongNguyễn <satthu147258369@gmail.com>
2019-11-01 20:16:15 +0700

    first commit

---
+++ Controller/ContactService.cs
@@ -0,0 +1,83 @@
+﻿using AppG2.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppG2.Controller
+{
+    public class ContactService
+    {
+        public static List<Contact> GetAllContact(string pathDataFile)
+        {
+            if (File.Exists(pathDataFile))
+            {
+                List<Contact> listContact = new List<Contact>();
+                //Mở file đọc hết toàn bộ các dòng tỏng file xong đóng file lại
+                var listLines = File.ReadAllLines(pathDataFile);
+
+                foreach (var lines in listLines)
+                {
+                    var rs = lines.Split(new char[] { '#' });
+                    Contact contact = new Contact
+                    {
+                        Id = rs[0],
+                        Name = rs[1],
+                        Phone = rs[2],
+                        Email = rs[3],
+                    };
+                    listContact.Add(contact);
+                }
+                return listContact;
+            }
+            else return null;
+        }
+        
+        public static List<Contact> GetContactBySearch(string originalText, string pathDataFile)
+        {
+            string text = originalText.ToLower();
+            List<Contact> newListContact = new List<Contact>();
+            if (!text.Equals(""))
+            {
+                List<Contact> listContact = GetAllContact(pathDataFile);
+                
+                foreach (var item in listContact)
+                {
+                    if (item.Name.ToLower().Contains(text) || item.Phone.ToLower().Contains(text) || item.Email.ToLower().Contains(text))
+                    {
+                        newListContact.Add(item);
+                    }
+                }
+                return newListContact;
+            } else
+            {
+                return GetAllContact(pathDataFile);
+            }
+
+        }
+
+        public static List<Contact> GetContactInAlphabetic(string text, string pathDataFile)
+        {
+            List<Contact> newListContact = new List<Contact>();
+            if (!text.Equals("All"))
+            {
+                List<Contact> listContact = GetAllContact(pathDataFile);
+
+                foreach (var item in listContact)
+                {
+                    if (String.Compare(item.FirstCharacter, text)>=0)
+                    {
+                        newListContact.Add(item);
+                    }
+                }
+                return newListContact;
+            }
+            else
+            {
+                return GetAllContact(pathDataFile);
+            }
+        }
+    }
+}
---
+++ View/frmContact.cs
@@ -0,0 +1,159 @@
+﻿using AppG2.Controller;
+using AppG2.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppG2.View
+{
+    public partial class frmContact : Form
+    {
+        #region Path data file
+        string pathContactFile;
+        #endregion
+
+        public frmContact()
+        {
+            InitializeComponent();
+            pathContactFile = Application.StartupPath + @"\Data\contact.txt";
+
+            bdsContact.DataSource = null;
+            dtgContact.AutoGenerateColumns = false;
+
+            var listContactNoSort = ContactService.GetAllContact(pathContactFile);
+            var listContact = listContactNoSort.OrderBy(x => x.Name).ToList();
+            if (listContact == null)
+                throw new Exception("Khong co lien lac nao");
+            else
+            {
+                bdsContact.DataSource = listContact;
+            }
+            dtgContact.DataSource = bdsContact;
+            AddNewLabel();
+        }
+
+        public void AddNewLabel()
+        {
+            flowLayoutPanel1.Controls.Clear();
+            List<string> listLabelDuplicate = new List<string>();
+            var listContactNoSort = ContactService.GetAllContact(pathContactFile);
+
+            //Them vao mang cac chu cai dau tien cua name
+            foreach (var item in listContactNoSort)
+            {
+                listLabelDuplicate.Add(item.FirstCharacter);
+            }
+
+            //Loai bo cac phan tu trung nhau
+            List<String> labels = listLabelDuplicate.Distinct().ToList();
+
+            //Sap xep lai mang
+            labels.Sort();
+
+            //Tao label moi 
+            for (int i = 0; i < labels.Count; i++)
+            {
+                Label lbl = new Label();
+                lbl.Text = labels[i];
+                lbl.Click += new System.EventHandler(this.Label_Click);
+                flowLayoutPanel1.Controls.Add(lbl);
+            }
+        }
+
+        private void BtnThem_Click(object sender, EventArgs e)
+        {
+            var f = new frmContactInfo(null); ;
+            if (f.ShowDialog() == DialogResult.OK)
+            {
+                //Tiến hành nạp lại dữ liệu lên lưới
+                var newContactListNoSort = ContactService.GetAllContact(pathContactFile);
+                List<Contact> newContactList = newContactListNoSort.OrderBy(x => x.Name).ToList();
+                bdsContact.DataSource = newContactList;
+                bdsContact.ResetBindings(true);
+                AddNewLabel();
+            }
+        }
+
+        private void BtnSua_Click(object sender, EventArgs e)
+        {
+            var contact = bdsContact.Current as Contact;
+            if (contact != null)
+            {
+                var f = new frmContactInfo(contact);
+                if (f.ShowDialog() == DialogResult.OK)
+                {
+                    //Tiến hành nạp lại dữ liệu lên lưới
+                    var newContactListNoSort = ContactService.GetAllContact(pathContactFile);
+                    List<Contact> newContactList = newContactListNoSort.OrderBy(x => x.Name).ToList();
+                    bdsContact.DataSource = newContactList;
+                    bdsContact.ResetBindings(true);
+                    AddNewLabel();
+                }
+            }
+        }
+
+        private void BtnXoa_Click(object sender, EventArgs e)
+        {
+            var rs = MessageBox.Show("Bạn có chắc muốn xóa dữ liệu này không?",
+                "Thông báo",
+                MessageBoxButtons.OKCancel,
+                MessageBoxIcon.Warning);
+            if (rs == DialogResult.OK)
+            {
+                //Viết code xóa dữ liệu ở đây
+                var contact = (Contact)bdsContact.Current;
+
+                //Lấy toàn bộ nội dung trong bds ra list
+                List<Contact> listContact = (List<Contact>)bdsContact.DataSource;
+                //Xóa mục đc chọn
+                listContact.Remove(contact);
+
+                //set lại bds = list mới
+                bdsContact.DataSource = listContact;
+                //hiển thị lại bds
+                bdsContact.ResetBindings(true);
+
+                // xóa trong file
+                String delete = contact.Id + "#" + contact.Name + "#" + contact.Phone + "#" + contact.Email;
+                var Lines = File.ReadAllLines(pathContactFile);
+                var newLines = Lines.Where(line => !line.Contains(delete));
+                File.WriteAllLines(pathContactFile, newLines);
+                //Ép kiểu 2 bdsQuaTrinhHocTap.Current as HistoryLearning
+                MessageBox.Show("Bạn đã xóa thành công. Name: " + contact.Name);
+            }
+            else
+            {
+                MessageBox.Show("Bạn đã không xóa");
+            }
+        }
+
+        private void TxtSearch_TextChanged(object sender, EventArgs e)
+        {
+            var contactListNoSort = ContactService.GetContactBySearch(txtSearch.Text, pathContactFile);
+            //var newContactList = contactListNoSort.OrderBy(x => x.Name).ToList();
+            bdsContact.DataSource = contactListNoSort;
+            bdsContact.ResetBindings(true);
+        }
+
+        private void BtnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Label_Click(object sender, EventArgs e)
+        {
+            var labelName = ((Label)sender).Text;
+            var listContactNoSort = ContactService.GetContactInAlphabetic(labelName, pathContactFile);
+            var newContactList = listContactNoSort.OrderBy(x => x.Name).ToList();
+            bdsContact.DataSource = newContactList;
+            bdsContact.ResetBindings(true);
+        }
+    }
+}
